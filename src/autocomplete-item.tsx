a16d23ae03226c7e06a002c58ee2c5330f953bf2
daliudzius <46355846+daliudzius@users.noreply.github.com>
2022-02-16 21:20:53 -0800

    remove always show prop

--- src/autocomplete-item.tsx
+++ src/autocomplete-item.tsx
@@ -14,7 +14,6 @@ export interface AutoCompleteItemProps extends FlexProps {
   value: any;
   label?: string;
   fixed?: boolean;
-  alwaysShow?: boolean;
   _focus?: CSSObject | any;
   disabled?: boolean;
   _fixed?: CSSObject;
