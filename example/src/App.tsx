c41271f99d82491819dfbb390d32cf12971b5c57
Kyle Slusser <Kysluss@aol.com>
2025-08-12 19:48:39 -0400

    chore: update dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,13 @@
 import {
-  Box, 
+  Box,
+  Button,
+  InputGroup, 
 } from "@chakra-ui/react"
 import { AutoComplete, 
   AutoCompleteInput, 
   AutoCompleteList, 
   AutoCompleteItem} from '../../src/index';
+import { useCallback, useEffect, useState } from "react";
 
 const options = [
   { label: "apple", value: "one" },
@@ -14,17 +17,33 @@ const options = [
   { label: "japan", value: "five" },
 ];
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState<any[]>([]);
+
+  const loadItems = useCallback(() => {
+    setIsLoading(true);
+
+    sleep(3000)
+      .then(() => {
+        setIsLoading(false);
+        setItems(options);
+      })
+  }, [setIsLoading, setItems]);
   return (
-    <Box textAlign="center" fontSize="xl" pt="30vh" w='300px' mx='auto' justifyContent='center' alignItems='center'>
+    <Box border="1px solid #999" textAlign="center" fontSize="xl" pt="30vh" w='300px' mx='auto' justifyContent='center' alignItems='center'>
+      <Button onClick={loadItems}>Trigger Loading State</Button>
       <AutoComplete
         openOnFocus
         rollNavigation
         listAllValuesOnFocus 
-        closeOnBlur={false}>
+        closeOnBlur={false} 
+        isLoading={isLoading}>
           <AutoCompleteInput variant="subtle" />
           <AutoCompleteList>
-            {options.map(option => (
+            {items.map(option => (
               <AutoCompleteItem
                 key={`option-${option.value}`}
                 value={{ title: `${option.value}` }}
@@ -35,6 +54,25 @@ export default function Page() {
             ))}
         </AutoCompleteList>
       </AutoComplete>
+
+<Box mt={5}>
+  <AutoComplete>
+    <InputGroup endElement={"X"}>
+      <AutoCompleteInput variant="subtle" />
+    </InputGroup>
+          <AutoCompleteList>
+            {items.map(option => (
+              <AutoCompleteItem
+                key={`option-${option.value}`}
+                value={{ title: `${option.value}` }}
+                // getValue={a => a.title}
+                label={option.label}
+                textTransform="capitalize"
+              />
+            ))}
+        </AutoCompleteList>
+  </AutoComplete>
+  </Box>
     </Box>
   )
 }
